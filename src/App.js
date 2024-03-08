6814b2c86f4fe6cb245ad472ddcad02b2bc08ae2
Chamas111 <ahmad.chamas87@gmail.com>
2024-03-08 22:35:52 +0100

    edit in some files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App">hello World</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
