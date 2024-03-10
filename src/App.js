862df2d990c455996d3a64793d0734b0c03a50e9
Chamas111 <ahmad.chamas87@gmail.com>
2024-03-10 10:57:35 +0100

    edit in some files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+
+      <Footer />
     </div>
   );
 }
